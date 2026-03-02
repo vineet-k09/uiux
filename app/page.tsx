101449bc3e5269603d21df2fa23066c5d6206260
ShreyaMandekar <mandekarofficial@gmail.com>
2026-03-02 23:36:25 +0530

    Added Landing page animations and Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Navbar from "@/components/layout/Navbar"
 import HeroSection from "@/components/sections/HeroSection"
+import LandingPage from "./landing/page"
+import Footer from "@/components/layout/Footer"
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col bg-linear-to-br from-zinc-950 via-black to-purple-800 text-white">
       <Navbar />
-      <HeroSection />
+      <LandingPage/>
+      <Footer/>
+      {/* <HeroSection /> */}
     </div>
   )
 }
---
+++ components/sections/Land1.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import React, { useLayoutEffect, useRef } from 'react';
+import Image from 'next/image';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+export default function HomeHeader() {
+  const background = useRef<HTMLDivElement | null>(null);
+  const introImage = useRef<HTMLDivElement | null>(null);
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: true,
+        start: 'top',
+        end: '+=500px',
+      },
+    });
+
+    timeline
+      .from(background.current, { clipPath: 'inset(15%)' })
+      .to(introImage.current, { height: '200px' }, 0);
+  }, []);
+
+  return (
+    <div className="relative w-full flex justify-center">
+      
+      {/* Background Image */}
+      <div
+        ref={background}
+        className="absolute w-full h-[140vh] brightness-[0.6]"
+      >
+        <Image
+          src="/landing_centre1.png"
+          alt="background image"
+          fill
+          priority
+          className="object-cover"
+        />
+      </div>
+
+      {/* Intro Section */}
+      <div className="relative mt-[35vh] flex justify-center">
+        
+        {/* Intro Image */}
+        <div
+          ref={introImage}
+          className="absolute w-[350px] h-[475px] brightness-[0.7]"
+        >
+          <Image
+            src="/landing_centre2.png"
+            alt="intro image"
+            fill
+            priority
+            className="object-cover object-top"
+          />
+        </div>
+
+        {/* Text */}
+        <h1 className="z-[3] text-white text-[3vw] text-center whitespace-nowrap">
+          Building Smarter Solutions for a Digital-First World
+        </h1>
+
+      </div>
+    </div>
+  );
+}
