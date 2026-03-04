df5649cf5aee2f1066aa781411f056293a596106
Vineet <vineetkushwaha6325@gmail.com>
2026-03-04 13:04:41 +0530

    tested loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/layout/Navbar"
+import IntroAnimation from "@/components/motion/Loader"
 import HeroSection from "@/components/sections/HeroSection"
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col bg-linear-to-br from-zinc-950 via-black to-purple-800 text-white">
+      <IntroAnimation />
       <Navbar />
       <HeroSection />
     </div>
