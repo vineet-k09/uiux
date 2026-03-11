b378faf8b656d682d62122b73b4b41ba74264914
Vineet Kushwaha <vineetkushwaha6325@gmail.com>
2026-03-11 11:12:30 +0530

    fixed the trigers for animation

--- components/sections/home/ParallaxCard_Section/parallaxCards copy.tsx
+++ components/sections/home/ParallaxCard_Section/parallaxCards copy.tsx
@@ -95,7 +95,7 @@ export default function ParallaxCardstrial() {
             snap: { innerText: 1 },
             scrollTrigger: {
               trigger: num,
-              start: "top 8 0%",
+              start: "top 80%",
             },
           }
         )
