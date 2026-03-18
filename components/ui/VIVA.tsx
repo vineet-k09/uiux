baf1c58d4b3c9cad7c902cb875d84c56602dd00a
Shreya Mandekar <mandekarofficial@gmail.com>
2026-03-18 10:03:31 +0000

    removed some why us data

--- components/ui/VIVA.tsx
+++ components/ui/VIVA.tsx
@@ -3,9 +3,7 @@ import globals from "../../data/globals.json";
 export default function VIVA() {
   return (
     <div>
-      <h1 className="md:text-4xl text-2xl font-extrabold 
-      bg-linear-to-r from-[#ff1a1a] via-[#ff2e63] to-[#8e2de2] 
-      bg-clip-text text-transparent">
+      <h1 className="md:text-4xl text-2xl font-extrabold bg-linear-to-r from-[#ff1a1a] via-[#ff2e63] to-[#8e2de2] bg-clip-text text-transparent">
         {globals.title}
       </h1>
     </div>
